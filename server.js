6eeab10b20b00e68a1279867ec5418ba655676cb
Jhoy1992 <jhonatanascari@gmail.com>
2020-10-07 11:00:00 -0300

    feat: improve stream logic

--- server.js
+++ server.js
@@ -19,27 +19,25 @@ app.get("/", (req, res) => {
 
 app.get("/videos/:name", (req, res) => {
   const path = `assets/${req.params.name}`;
+
   const stat = fs.statSync(path);
+
   const fileSize = stat.size;
+
   const range = req.headers.range;
 
-  if (!range) {
-    const headers = { "Content-Length": fileSize, "Content-Type": "video/mp4" };
+  const start = Number((range || "").replace(/bytes=/, "").split("-")[0]);
 
-    res.writeHead(headers);
-    fs.createReadStream(path).pipe(res);
-  }
+  const end = fileSize - 1;
 
-  const parts = range.replace(/bytes=/, "").split("-");
-  const start = parseInt(parts[0], 10);
-  const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-  const chunksize = end - start + 1;
+  const chunkSize = end - start + 1;
+
   const file = fs.createReadStream(path, { start, end });
 
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${fileSize}`,
     "Accept-Ranges": "bytes",
-    "Content-Length": chunksize,
+    "Content-Length": chunkSize,
     "Content-Type": "video/mp4",
   };
 
